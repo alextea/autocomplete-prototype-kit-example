ce71deee144da346adaf9774b79497567aac9b5c
Alex Torrance <alextorrace@wearefuturegov.com>
2021-08-17 21:01:53 +0100

    Enable searching by synonyms using the value attribute

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -12,6 +12,23 @@ $(document).ready(function () {
 
   accessibleAutocomplete.enhanceSelectElement({
     defaultValue: '',
+    source: (query, populateResults) => {
+      const options = selectElement.querySelectorAll('option')
+      let results = []
+
+      options.forEach(
+        (opt, i, list) => {
+          let queryRegExp = new RegExp(query.trim(), 'i')
+          let value = opt.getAttribute('value')
+          let text = opt.innerText.trim()
+          if (queryRegExp.test(value) || queryRegExp.test(text)) {
+            results.push(text)
+          }
+        }
+      )
+
+      populateResults(results)
+    },
     selectElement: selectElement
   })
 })
